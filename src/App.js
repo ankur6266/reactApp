78a75f8245cfa600fb1a83ef2bc00754c303f726
ankur6266 <ankursaxenaankur0@gmail.com>
2023-02-16 16:16:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './header';
+import Footer from './footer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App file</h1>
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
